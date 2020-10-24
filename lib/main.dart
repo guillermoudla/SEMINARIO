b9d58a9efc86537544bbce87128085c332d6ad8b
guillermoudla <73011439+guillermoudla@users.noreply.github.com>
2020-10-24 10:19:59 -0500

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,97 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+void main() =>
+    runApp(new MaterialApp(
+      title: "Camara app",
+      home: LandingScreen(),
+    ));
+
+class LandingScreen extends StatefulWidget {
+  @override
+  _LandingScreenState createState() => _LandingScreenState();
+}
+
+class _LandingScreenState extends State<LandingScreen> {
+  File imageFile;
+
+  _openGallery (BuildContext context) async {
+    var  picture= await ImagePicker.pickImage(source: ImageSource.gallery);
+    this.setState((){
+      imageFile=picture;
+    });
+    Navigator.of(context).pop();
+  }
+
+  _openCamera(BuildContext context) async {
+    var  picture= await ImagePicker.pickImage(source: ImageSource.camera);
+    this.setState((){
+      imageFile=picture;
+    });
+    Navigator.of(context).pop();
+  }
+
+  Future<void> _showChoiceDialog(BuildContext context) {
+    return showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text("Seleccione"),
+            content: SingleChildScrollView(
+              child: ListBody(
+                children: <Widget>[
+                  GestureDetector(
+                    child: Text("Galeria"),
+                    onTap: () {
+                      _openGallery(context);
+                    },
+                  ),
+                  Padding(padding: EdgeInsets.all(8.0),),
+                  GestureDetector(
+                    child: Text("Camara"),
+                    onTap: () {
+                      _openCamera(context);
+                    },
+                  )
+                ],
+              ),
+            ),
+          );
+        });
+  }
+  //retorna  texto si no se ha seleccionado alguna imagen
+  Widget _decideImageView(){
+    if(imageFile==null){
+      return Text("No se ha seleccionado una imagen");
+    }
+    else{
+      return  Image.file(imageFile,width: 400, height: 400);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Demo Acceso Camara/Galeria"),
+      ),
+      body: Container(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              _decideImageView(),
+              RaisedButton(
+                onPressed: () {
+                  _showChoiceDialog(context);
+                },
+                child: Text("Selecciona la Imagen"),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
